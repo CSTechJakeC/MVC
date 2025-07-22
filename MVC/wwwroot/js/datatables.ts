be857e2c674f0f7ca6a4e7cab3e9db6eb842f42a
cstechjakecarabott <cstech.jake.carabott@gmail.com>
2025-07-22 10:30:18 +0200

    Add filtering

--- MVC/wwwroot/js/datatables.ts
+++ MVC/wwwroot/js/datatables.ts
@@ -27,12 +27,35 @@
 
             const columns = Object.keys(data[0]).map(key => ({
                 title: Mapping[key] || key,
-                data: key
+                data: key,
+                name: key
             }));
 
-            this.$context.DataTable({
+             const table = this.$context.DataTable({
                 data: data,
-                columns: columns
+                columns: columns,
+                dom: '<"top">t<"bottom"lip>'
+            });
+
+            const $input = $("#customsearchinput");
+            const $select = $("#customsearchcolumn");
+
+            $input.on("keyup", function () {
+                const keyword = ($input.val() as string).toLowerCase();
+                const column = $select.val();
+
+                table.search('').columns().search('');
+
+                if (column == "all") {
+                    table.search(keyword).draw();
+                } else {
+                    const columnindex = table.column(`${column}:name`).index();
+                    table.column(columnindex).search(keyword).draw();
+                }
+            });
+
+            $select.on("change", function () {
+                $input.trigger("keyup"); 
             });
         }
 

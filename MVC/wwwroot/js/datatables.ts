54ce5f03997896cbafbf6332771b05da5495c700
cstechjakecarabott <cstech.jake.carabott@gmail.com>
2025-07-23 14:08:41 +0200

    Code Clean Up

--- MVC/wwwroot/js/datatables.ts
+++ MVC/wwwroot/js/datatables.ts
@@ -18,89 +18,17 @@
         private renderTable(data: any[]): void {
             if (data.length == 0) return;
 
-            const Mapping: { [key: string]: string } = {
-                locales_Id: "Locales",
-                pageFriendlyName: "Page Name",
-                labelFriendlyName: "Label Name",
-                text: "Text"
-            };
+            const columns = this.mapColumnNames(data);
 
-            const columns = Object.keys(data[0]).map(key => ({
-                title: Mapping[key] || key,
-                data: key,
-                name: key
-            }));
+            this.pushActionsColumn(columns);
+            
+            const table = this.setTable(data,columns);
 
-            (columns as any[])  .push({
-                title: "Actions",
-                data: null,
-                sortable: false,
-                orderable: false,
-                defaultContent: `
-                <div class="action-buttons">
-                    <button class="btn btn-sm btn-outline-primary"><i class="fa-solid fa-download"></i></button>
-                    <button class="btn btn-sm btn-outline-secondary"><i class="fa-brands fa-twitter"></i></button>
-                    <button class="btn btn-sm btn-outline-dark"><i class="fa-solid fa-eye"></i></button>
-                </div>`
-            });
+            this.setNavRow();
+     
 
-
-
-             const table = this.$context.DataTable({
-                data: data,
-                columns: columns,
-                dom: '<"top">t<"bottom custom-footer"lpi>'
-            });
-
-            setTimeout(() => {
-
-                const $goto = $(`
-                <div class="goto-page-controls" style="display: inline-flex; align-items: center; gap: 0.25rem;">
-                  Go to page:
-                  <input type="number" id="gotoPageInput" min="1"
-                         class="form-control form-control-sm d-inline-block"
-                         style="width: 60px;" />
-                  <button id="gotoPageBtn" class="btn btn-outline-secondary btn-sm">Go</button>
-                </div>
-              `);
-
-                //Detaching
-                const $pag = $(".custom-footer .dataTables_paginate").detach();
-                const $len = $(".custom-footer .dataTables_length").detach();
-
-             
-                const $navRow = $('<div class="footer-nav-row"></div>').css({
-                    display: 'flex',
-                    width: '100%',
-                    alignItems: 'center',
-                    marginTop: '0.5rem'
-                });
-
-                //Reattaching to new Row
-                $navRow.append($('<div></div>').css('flex', '1'));
-                $navRow.append($len.css({ margin: 0 }));
-          
-                $navRow.append($pag.css({ margin: '0 auto' }));
-
-               
-                $navRow.append(
-                    $('<div></div>').css({
-                        flex: '1',
-                        display: 'flex',
-                        justifyContent: 'flex-end'
-                    }).append($goto)
-                );
-
-           
-                $(".custom-footer .dataTables_info").before($navRow);
-            }, 0);
-
-
- 
-
-
-            const $input = $("#customsearchinput");
-            const $select = $("#customsearchcolumn");
+            const $input = $("#customSearchInput");
+            const $select = $("#customSearchColumn");
 
             $input.on("keyup", function () {
                 const keyword = ($input.val() as string).toLowerCase();
@@ -139,6 +67,94 @@
             });
         }
 
+        private mapColumnNames(data: any[]) {
+            const Mapping: { [key: string]: string } = {
+            locales_Id: "Locales",
+            pageFriendlyName: "Page Name",
+            labelFriendlyName: "Label Name",
+            text: "Text"
+            };
+
+            const columns = Object.keys(data[0]).map(key => ({
+                title: Mapping[key] || key,
+                data: key,
+                name: key
+            }));
+
+            return columns;
+        }
+
+        private pushActionsColumn(columns: any[]) {
+            (columns as any[]).push({
+                title: "Actions",
+                data: null,
+                sortable: false,
+                orderable: false,
+                defaultContent: `
+                <div class="action-buttons">
+                    <button class="btn btn-sm btn-outline-primary"><i class="fa-solid fa-download"></i></button>
+                    <button class="btn btn-sm btn-outline-secondary"><i class="fa-brands fa-twitter"></i></button>
+                    <button class="btn btn-sm btn-outline-dark"><i class="fa-solid fa-eye"></i></button>
+                </div>`
+            });
+        }
+
+        private setTable(data: any[], columns: any[]) {
+            const table = this.$context.DataTable({
+                data: data,
+                columns: columns,
+                dom: '<"top">t<"bottom custom-footer"lpi>'
+            });
+
+            return table;
+        }
+
+        private setNavRow() {
+            setTimeout(() => {
+
+                const $goto = $(`
+                    <div class="goto-page-controls" style="display: inline-flex; align-items: center; gap: 0.25rem;">
+                      Go to page:
+                      <input type="number" id="gotoPageInput" min="1"
+                             class="form-control form-control-sm d-inline-block"
+                             style="width: 60px;" />
+                      <button id="gotoPageBtn" class="btn btn-outline-secondary btn-sm">Go</button>
+                    </div>
+              `);
+
+                //Detaching
+                const $pag = $(".custom-footer .dataTables_paginate").detach();
+                const $len = $(".custom-footer .dataTables_length").detach();
+
+
+                const $navRow = $('<div class="footer-nav-row"></div>').css({
+                    display: 'flex',
+                    width: '100%',
+                    alignItems: 'center',
+                    marginTop: '0.5rem'
+                });
+
+                //Reattaching to new Row
+                $navRow.append($('<div></div>').css('flex', '1'));
+                $navRow.append($len.css({ margin: 0 }));
+
+                $navRow.append($pag.css({ margin: '0 auto' }));
+
+
+                $navRow.append(
+                    $('<div></div>').css({
+                        flex: '1',
+                        display: 'flex',
+                        justifyContent: 'flex-end'
+                    }).append($goto)
+                );
+
+
+                $(".custom-footer .dataTables_info").before($navRow);
+            }, 0);
+        }
+
+
     }
 
     $(function () {
@@ -147,7 +163,4 @@
             new DataScreen.Table($("#myTable"));
         }
     });
-
-
-
 }
